cff6c63549caaa382ef588992b338882678475e9
Wangxy <xtwxy@hotmail.com>
2017-10-03 14:19:32 +0800

    add waitpid test.

--- apue/producer_consumer/src/main.c
+++ apue/producer_consumer/src/main.c
@@ -4,19 +4,19 @@
 #include <string.h>
 
 #ifdef NOPRINT
-  #define PRINTF(format, ... )
+#define PRINTF(format, ... )
 #else
-  #define PRINTF printf
+#define PRINTF printf
 #endif
 
 struct shared_queue {
-    pthread_mutex_t lock;
-    pthread_cond_t full;
-    pthread_cond_t empty;
-    size_t produced;
-    size_t consumed;
-    size_t max_count;
-    size_t capacity;
+  pthread_mutex_t lock;
+  pthread_cond_t full;
+  pthread_cond_t empty;
+  size_t produced;
+  size_t consumed;
+  size_t max_count;
+  size_t capacity;
 };
 
 static pthread_once_t queue_init_once = PTHREAD_ONCE_INIT;
@@ -24,82 +24,85 @@ static pthread_once_t queue_destroy_once = PTHREAD_ONCE_INIT;
 static struct shared_queue queue;
 
 void init_queue() {
-    pthread_mutex_init(&queue.lock, NULL);
-    pthread_cond_init(&queue.full, NULL);
-    pthread_cond_init(&queue.empty, NULL);
-    PRINTF("lock and shared_queue initialized.\n");
+  pthread_mutex_init(&queue.lock, NULL);
+  pthread_cond_init(&queue.full, NULL);
+  pthread_cond_init(&queue.empty, NULL);
+  PRINTF("lock and shared_queue initialized.\n");
 }
 
 void destroy_queue() {
-    pthread_mutex_destroy(&queue.lock);
-    pthread_cond_destroy(&queue.full);
-    pthread_cond_destroy(&queue.empty);
-    PRINTF("lock and shared_queue destroyed.\n");
+  pthread_mutex_destroy(&queue.lock);
+  pthread_cond_destroy(&queue.full);
+  pthread_cond_destroy(&queue.empty);
+  PRINTF("lock and shared_queue destroyed.\n");
 }
 
 void* producer_thread(void *p) {
-    struct shared_queue *q = (struct shared_queue *)p;
-    for(; q->produced != q->max_count; ) {
-        pthread_mutex_lock(&(q->lock));
-        if((q->produced - q->consumed) < q->capacity) {
-            q->produced++;
-            PRINTF("produced: %lu\n", q->produced);
-            PRINTF("producer(produced, consumed) => (%lu, %lu)\n", q->produced, q->consumed);
-            pthread_cond_signal(&(q->empty));
-        } else {
-            PRINTF("full: wait for consuming.\n");
-            pthread_cond_wait(&(q->full), &(q->lock));
-        }
-        pthread_mutex_unlock(&(q->lock));
+  struct shared_queue *q = (struct shared_queue *)p;
+  for(; q->produced != q->max_count; ) {
+    pthread_mutex_lock(&(q->lock));
+    if((q->produced - q->consumed) < q->capacity) {
+      q->produced++;
+      PRINTF("produced: %lu\n", q->produced);
+      PRINTF("producer(produced, consumed) => (%lu, %lu)\n", q->produced, q->consumed);
+      pthread_cond_signal(&(q->empty));
+    } else {
+      PRINTF("full: wait for consuming.\n");
+      pthread_cond_wait(&(q->full), &(q->lock));
     }
-    return p;
+    pthread_mutex_unlock(&(q->lock));
+  }
+  return p;
 }
 
 void* consumer_thread(void *p) {
-    struct shared_queue *q = (struct shared_queue *)p;
-    for(; q->consumed != q->max_count; ) {
-        pthread_mutex_lock(&(q->lock));
-        if(q->produced != q->consumed) {
-            q->consumed++;
-            PRINTF("consumed: %lu\n", q->consumed);
-            PRINTF("consumer(produced, consumed) => (%lu, %lu)\n", q->produced, q->consumed);
-            pthread_cond_signal(&(q->full));
-        } else {
-            PRINTF("empty: wait for producing.\n");
-            pthread_cond_wait(&(q->empty), &(q->lock));
-        }
-        pthread_mutex_unlock(&(q->lock));
+  struct shared_queue *q = (struct shared_queue *)p;
+  for(; q->consumed != q->max_count; ) {
+    pthread_mutex_lock(&(q->lock));
+    if(q->produced != q->consumed) {
+      q->consumed++;
+      PRINTF("consumed: %lu\n", q->consumed);
+      PRINTF("consumer(produced, consumed) => (%lu, %lu)\n", q->produced, q->consumed);
+      pthread_cond_signal(&(q->full));
+    } else {
+      PRINTF("empty: wait for producing.\n");
+      pthread_cond_wait(&(q->empty), &(q->lock));
     }
-    return p;
+    pthread_mutex_unlock(&(q->lock));
+  }
+  return p;
 }
 
 int main(int argc, char* argv[]) {
-    if(argc < 3) {
-        PRINTF("Usage: %s <queue capacity> <max count to produce>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+  for(int i = 0; i < argc; ++i) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  if(argc < 3) {
+    PRINTF("Usage: %s <queue capacity> <max count to produce>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-    memset(&queue, 0, sizeof(queue));
+  memset(&queue, 0, sizeof(queue));
 
-    pthread_once(&queue_init_once, init_queue);
-    queue.capacity = atoi(argv[1]);
-    queue.max_count = atoi(argv[2]);
+  pthread_once(&queue_init_once, init_queue);
+  queue.capacity = atoi(argv[1]);
+  queue.max_count = atoi(argv[2]);
 
-    pthread_attr_t attr;
-    void *result;
-    pthread_t producer;
-    pthread_t consumer;
-    pthread_attr_init(&attr);
+  pthread_attr_t attr;
+  void *result;
+  pthread_t producer;
+  pthread_t consumer;
+  pthread_attr_init(&attr);
 
-    pthread_create(&producer, &attr, &producer_thread, &queue);
-    pthread_create(&consumer, &attr, &consumer_thread, &queue);
-    
-    pthread_join(producer, &result);
-    pthread_join(consumer, &result);
-    pthread_attr_destroy(&attr);
+  pthread_create(&producer, &attr, &producer_thread, &queue);
+  pthread_create(&consumer, &attr, &consumer_thread, &queue);
 
-    pthread_once(&queue_destroy_once, destroy_queue);
+  pthread_join(producer, &result);
+  pthread_join(consumer, &result);
+  pthread_attr_destroy(&attr);
 
-    return EXIT_SUCCESS;
+  pthread_once(&queue_destroy_once, destroy_queue);
+
+  return EXIT_SUCCESS;
 }
 
---
+++ apue/waitpid/src/main.c
@@ -0,0 +1,37 @@
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <config.h>
+
+int main(int argc, char* argv[]) {
+  pid_t pid;
+  int status;
+  
+  if(argc != 4) {
+    printf("Usage:\n%s \"<cmd line>\"\n", argv[1]);
+  }
+
+  if((pid = fork()) < 0) { // error.
+    status = -1;
+    printf("error\n");
+  } else if(pid == 0) { // child.
+    printf("%s %s %s\n", argv[1], argv[2], argv[3]);
+    execl(argv[1], argv[1], argv[2], argv[3], (char *)0);
+    printf("child error\n");
+    _exit(127);
+  } else { // parent.
+    printf("parent\n");
+    while(waitpid(pid, &status, 0) < 0) {
+      if(errno != EINTR) {
+	status = -1;
+        printf("parent error\n");
+	break;
+      }
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
