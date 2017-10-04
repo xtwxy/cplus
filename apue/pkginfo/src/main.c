d7b90055cec689d51692efb4c5ec647b7f9b5a9a
Wangxy <xtwxy@hotmail.com>
2017-10-04 08:55:16 +0800

    add select tests.

--- apue/pkginfo/src/main.c
+++ apue/pkginfo/src/main.c
@@ -4,22 +4,22 @@
 
 int main(int argc, char* argv[]) {
 
-	printf("package\t\t: '%s' \n"
-			"package name\t: '%s' \n"
-			"version\t\t: '%s' \n"
-			"bug report\t: '%s' \n"
-			"package version\t: '%s,' \n"
-			"version string\t: '%s' \n"
-			"tar name\t: '%s' \n"
-			"url\t\t: '%s' \n",
-			PACKAGE,
-			PACKAGE_NAME,
-			VERSION,
-			PACKAGE_BUGREPORT,
-			PACKAGE_VERSION,
-			PACKAGE_STRING,
-			PACKAGE_TARNAME,
-			PACKAGE_URL);
+  printf("package\t\t: '%s' \n"
+	 "package name\t: '%s' \n"
+	 "version\t\t: '%s' \n"
+	 "bug report\t: '%s' \n"
+	 "package version\t: '%s,' \n"
+	 "version string\t: '%s' \n"
+	 "tar name\t: '%s' \n"
+	 "url\t\t: '%s' \n",
+	 PACKAGE,
+	 PACKAGE_NAME,
+	 VERSION,
+	 PACKAGE_BUGREPORT,
+	 PACKAGE_VERSION,
+	 PACKAGE_STRING,
+	 PACKAGE_TARNAME,
+	 PACKAGE_URL);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
